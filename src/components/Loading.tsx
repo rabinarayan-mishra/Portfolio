30d1ae955855e7476342b90c645df7360e208283
Rabinarayan mishra <mishrarabi776@gmail.com>
2026-09-06 03:27:39 +0530

    Initial portfolio setup

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -50,7 +50,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          AV
+          RM
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
